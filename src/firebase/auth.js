308959b8fd23f977bf1ecf1641025bf24472bc6e
Lisbeth Lopez <lis2310.llla@gmail.com>
2022-07-08 11:21:28 -0500

    Borrando cambios

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,13 +1,1 @@
 
-import {
-    auth, sendEmailVerification,
-    createUserWithEmailAndPassword,
-
-} from './firebaseconfig.js';
-
-export const addUser=(email,password)=>createUserWithEmailAndPassword(auth, email, password);
-
-
-export const emailVerificationHandler = ()=> sendEmailVerification(auth.currentUser);
-
-  
