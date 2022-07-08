2acabc23300d9fff9f8c7fc3dbd534db08de4b41
Lisbeth Lopez <lis2310.llla@gmail.com>
2022-07-08 11:35:02 -0500

    Actualizando

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,1 +1,13 @@
+import {
+    auth, sendEmailVerification,
+    createUserWithEmailAndPassword,
 
+} from './firebaseconfig.js';
+
+export const addUser=async(email,password)=> {
+  return await createUserWithEmailAndPassword(auth, email, password) 
+}
+
+export const emailVerificationHandler = async()=> {
+   return await sendEmailVerification(auth.currentUser)   
+}
---
+++ src/firebase/firestore.js
@@ -0,0 +1,15 @@
+import {
+    db,
+    doc,
+    setDoc,
+    
+  } from './firebaseconfig.js';
+  
+  //Añadir nuevo usuario (Document) a users (colección)
+  export  async function createNewUser (name, email, userId){
+    await setDoc(doc(db, 'users', userId), {
+        name,
+        email
+    });
+    console.log('estoy llamando a createuser');
+}
