250385a48499eb5f22db8a6dc179d0f82ae5ee1a
Lisbeth Lopez <lis2310.llla@gmail.com>
2022-07-18 18:13:58 -0500

    Validando los campos

--- src/views/publications.js
+++ src/views/publications.js
@@ -3,51 +3,6 @@ import { localStorageCall } from '../componentes/sessionStorage.js';
 import { dateTime } from './timeago.js';
 import { shareImgPost } from '../firebase/storage.js';
 
-export const publicationView = () => {
-  console.log(dateTime());
-  const userObject = localStorageCall();
-  const publicationContent = `
-  <div class="namePhotoPublication">
-    <img src="${userObject.profilePhoto}" width = "50px">
-    <h2>${userObject.name}</h2>
-    <select id="privacyPostArea">
-            <option value="1">🌎 Público</option>
-            <option value="2">🔒 Solo yo </option>
-    </select>
-  </div>
-  <textarea placeholder="Escribe Algo ..." id='inputText'></textarea>
- 
-  <div class="divcamera">
-    <div class="inputFiles">
-      <label for="compartirImg"></label>
-      <input type="file"  id="compartirImg" >
-    </div>
-    <h4>Agrega una imagen</h4>
-    <div id="addImage">
-  </div>
-  </div>
-  
-  <div class="buttonGeneralPublication">
-    <button id = "publish" class="buttonPublication" type="submit">publicar</button>
-    <button id = "cancel" class="buttonPublication">cancelar</button>
-  </div>
-        `;
-  const publicationContainer = document.createElement('div');
-  publicationContainer.setAttribute('class', 'sectionPublication');
-  publicationContainer.innerHTML = publicationContent;
-
-  // Obteniendo botones de publicar y cancelar
-  const publishBtn = publicationContainer.querySelector('#publish');
-  publishBtn.addEventListener('click', addPublications);
-  const cancelBtn = publicationContainer.querySelector('#cancel');
-
-  const inputText = publicationContainer.querySelector('#compartirImg');
-  inputText.addEventListener('change', (addImage));
-
-  /* cancelBtn.addEventListener('click', deletePublications); */
-  return publicationContainer;
-};
-
 async function addPublications(e) {
   e.preventDefault();
 
@@ -55,25 +10,25 @@ async function addPublications(e) {
   const creator = localStorageCall().id;
 
   const contentPost = inputText.value;
-  
 
   const namecreator = localStorageCall().name;
   const photoCreator = localStorageCall().profilePhoto;
+  /** ******SUBIENDO IMAGENES AL STORAGE Y AL FIRESTORE************ */
+  const inputTypeFile = document.getElementById('compartirImg');
+  let urlImg = '';
+  if (inputTypeFile.value) {
+    const urlImage = inputTypeFile.files[0].name;
+    const file = inputTypeFile.files[0];
+    console.log(file);
+    await shareImgPost(urlImage, file);
+    urlImg = await shareImgPost(urlImage, file);
+  }
 
-  const urlImage = document.getElementById('compartirImg').files[0].name;
-  const file = document.getElementById('compartirImg').files[0];
+  /** ******SUBIENDO DATOS AL FIRESTORE************ */
 
-  console.log(file);
-  await shareImgPost(urlImage, file);
-  // setTimeout(() => {
-  //   const a=shareImgPost(urlImage, file);
-  //   console.log(a);
-  // }, 6000);
-  
-
-  const urlImg = await shareImgPost(urlImage, file);
-
-  loadPublications(creator, contentPost, urlImg, namecreator, photoCreator);
+  if (inputTypeFile.value || inputText.value) {
+    loadPublications(creator, contentPost, urlImg, namecreator, photoCreator);
+  }
 }
 
 function addImage() {
@@ -86,6 +41,7 @@ function addImage() {
   const imagen = document.createElement('img');
 
   const iconX = document.createElement('span');
+  imageContainer.setAttribute('id', 'deleteButtonImage');
   iconX.innerHTML = '✖';
 
   const read = new FileReader();
@@ -101,4 +57,66 @@ function addImage() {
   };
 
   read.readAsDataURL(file[0]);
+
+  /** *Borrando el preview de la imagen */
+  const deleteButtonImage = document.getElementById('deleteButtonImage');
+  deleteButtonImage.addEventListener('click', () => {
+    divAddImage.innerHTML = '';
+    const cleanInputFile = document.getElementById('compartirImg');
+    cleanInputFile.value = '';
+  });
 }
+
+export const publicationView = () => {
+  console.log(dateTime());
+  const userObject = localStorageCall();
+  const publicationContent = `
+  <div class="namePhotoPublication">
+    <img src="${userObject.profilePhoto}" width = "50px">
+    <h2>${userObject.name}</h2>
+    <select id="privacyPostArea">
+            <option value="1">🌎 Público</option>
+            <option value="2">🔒 Solo yo </option>
+    </select>
+  </div>
+  <form id="postForm">
+    <textarea placeholder="Escribe Algo ..." id='inputText'></textarea>
+  
+    <div class="divcamera">
+      <div class="inputFiles">
+        <label for="compartirImg"></label>
+        <input type="file"  id="compartirImg" >
+      </div>
+      <h4>Agrega una imagen</h4>
+      <div id="addImage">
+      </div>
+    </div>
+  </form>
+  
+  <div class="buttonGeneralPublication">
+    <button id = "publish" class="buttonPublication" type="submit">publicar</button>
+    <button id = "cancel" class="buttonPublication">cancelar</button>
+  </div>
+        `;
+  const publicationContainer = document.createElement('div');
+  publicationContainer.setAttribute('class', 'sectionPublication');
+  publicationContainer.innerHTML = publicationContent;
+
+  const postForm = publicationContainer.querySelector('#postForm');
+  // Obteniendo botones de publicar y cancelar
+  const publishBtn = publicationContainer.querySelector('#publish');
+  publishBtn.addEventListener('click', addPublications);
+
+  const cancelBtn = publicationContainer.querySelector('#cancel');
+  cancelBtn.addEventListener('click', () => {
+    postForm.reset();
+    const divAddImage = document.getElementById('addImage');
+    divAddImage.innerHTML = '';
+  });
+
+  const inputText = publicationContainer.querySelector('#compartirImg');
+  inputText.addEventListener('change', (addImage));
+
+  /* cancelBtn.addEventListener('click', deletePublications); */
+  return publicationContainer;
+};
